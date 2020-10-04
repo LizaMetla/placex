c461e62a9e408993af5825ea703fb5cdfcac9ff8
Liza <liza.metla@gmail.com>
2020-10-04 04:03:08 +0300

    Added paginator

--- rent/models.py
+++ rent/models.py
@@ -1,8 +1,13 @@
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
+from django.dispatch import receiver
+
+
 class User(AbstractUser):
     name = models.CharField(verbose_name='ФИО', max_length=50, null=True, blank=True)
     max_price = models.FloatField(verbose_name='Максимальная цена', null=True, blank=True)
@@ -11,6 +16,7 @@ class User(AbstractUser):
 
 
 class Advert(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Владелец')
     description = models.TextField(verbose_name='Описание')
     date_created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     date_updated = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
@@ -22,6 +28,17 @@ class Advert(models.Model):
 
     def get_main_image(self):
         return self.images.filter(is_main=True).first()
+
+@receiver(models.signals.post_delete, sender=Advert)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.source_file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
 
 
 class Image(models.Model):
--- rent/views.py
+++ rent/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 
 from rent.models import Advert
 
 
-class DefaultPageView(TemplateView):
+class DefaultPageView(ListView):
     template_name = 'rent/index.html'
+    paginate_by = 20
+    context_object_name = 'adverts_list'
+    def get_queryset(self):
+        return Advert.objects.all()
 
-    def get(self, request, *args, **kwargs):
-        adverts = Advert.objects.all()
-        return render(request, self.template_name, {'adverts_list': adverts})
 
 class AdvertDetailView(DetailView):
     template_name = "rent/advert-detail.html"
     model = Advert
+
+class MyAdvertsView(TemplateView):
+    template_name = ...
+    def get(self, request, *args, **kwargs):
+
+        return render(request, self.template_name, {})
