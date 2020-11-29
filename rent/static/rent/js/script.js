b9bedeb7e867bfd262c93ac69ca6b1fa36cccd50
Vlad Meleshko <vladik4.98@mail.ru>
2020-11-29 16:10:50 +0300

    feat: log-in window

--- rent/static/rent/js/script.js
+++ rent/static/rent/js/script.js
@@ -5,6 +5,7 @@ const costMax = document.querySelector('#id_cost_max');
 const costValueMin = document.querySelector('#cost_min_value');
 const costValueMax = document.querySelector('#cost_max_value');
 
+// filter bitton
 filterBtn.addEventListener('click', () => {
     if (searchForm.classList.contains('close')) {
         searchForm.classList.remove('close');
@@ -13,8 +14,7 @@ filterBtn.addEventListener('click', () => {
     }
 });
 
-// const costValue = document.createElement('p');
-
+// render costs
 function inputRange(cost, selector) {
     selector.textContent = cost;
 }
@@ -27,7 +27,33 @@ costMax.addEventListener('input', () => {
     inputRange(costMax.value, costValueMax);
 });
 
-
-
 inputRange(costMin.value, costValueMin);
 inputRange(costMax.value, costValueMax);
+
+// log-in window
+const logInBtn = document.querySelector('#log-in-btn');
+const logInWindow = document.querySelector('#log-in-window');
+const formContainer = document.querySelector('.log-in-form-container');
+const closeBtn = document.querySelector('.modal__close');
+
+logInBtn.addEventListener('click', () => {
+    if (logInWindow.classList.contains('close')) {
+        logInWindow.classList.remove('close');
+    }
+    if (formContainer.classList.contains('close')) {
+        formContainer.classList.remove('close');
+    }
+});
+
+function closeLogInWindow() {
+    logInWindow.classList.add('close');
+    formContainer.classList.add('close');
+}
+
+closeBtn.addEventListener('click', () => {
+    closeLogInWindow();
+});
+
+formContainer.addEventListener('click', () => {
+    closeLogInWindow();
+});
