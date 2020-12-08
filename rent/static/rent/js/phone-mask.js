529da668324816504154a187daaf6063a1d3c33f
Vlad Meleshko <vladik4.98@mail.ru>
2020-12-08 18:18:31 +0300

    delete: comments in phone-mask.js

--- rent/static/rent/js/phone-mask.js
+++ rent/static/rent/js/phone-mask.js
@@ -1,10 +1,6 @@
 // mask for phone
 var phone = document.querySelector('#phone');
 
-// phone.onclick = function() {
-//     phone.value = "+375";
-// }
-
 if (phone) {
   phone.addEventListener('click', () => {
     if (phone.value === '') {
@@ -55,33 +51,3 @@ if (phone) {
     }
   });
 }
-
-// phone.onkeydown = function() {
-//     var curLen = phone.value.length;
-    
-//     if (curLen < old){
-//       old--;
-//       return;
-//     }
-    
-//     if (curLen == 4) 
-//     	phone.value = phone.value + " (";
-      
-//     if (curLen == 8)
-//     	phone.value = phone.value + ") ";
-      
-//      if (curLen == 13)
-//     	phone.value = phone.value + "-"; 
-      
-//      if (curLen == 16)
-//     	phone.value = phone.value + "-";  
-      
-//      if (curLen > 18)
-//     	phone.value = phone.value.substring(0, phone.value.length - 1);
-      
-//     if (curLen !== 19) {
-//       phone.setCustomValidity('Номер телефона введен неверно! Пожалуйста, повторите ввод.');
-//     }
-
-//      old++;
-// }
